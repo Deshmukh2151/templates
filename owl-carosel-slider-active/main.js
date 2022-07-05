f47ff637b78e70a4db40e7c322c297d09d4cee92
Deshmukh2151 <rajendradeshmukh2151@gmail.com>
2022-07-05 16:08:15 +0530

    first commit

---
+++ owl-carosel-slider-active/main.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($){
+    "use strict"
+    $('#customers-testimonials').owlCarousel({
+        loop:true,
+        center:true,
+        items:3,
+        margin:0,
+        autoplay:false,
+        dots:true,
+        autoplayTimeout:8500,
+        smartSpeed:450,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }    
+    })
+})
